56ba1efd3af87e6f78bdbdf9794f8882bb65e734
JuliaGood <gudak.yuliya@gmail.com>
2021-02-09 20:12:31 +0200

    styling game. some CSS styles were added

--- src/Board.js
+++ src/Board.js
@@ -98,7 +98,14 @@ class Board extends Component {
   render() {
     // if the game is won, just show a winning msg & render nothing else
     if (this.state.hasWon) {
-      return <h1>YOU WON!!!</h1>
+      return (
+        <div className='Board-title'>
+          <div className='winner'>
+            <span className="neon-orange">YOU</span>
+            <span className="neon-blue">WON!</span>
+          </div>
+        </div>
+      )
     }
     // make table board
     let tableBoard = [];
@@ -117,11 +124,17 @@ class Board extends Component {
       tableBoard.push(<tr key={y}>{row}</tr>)
     }
     return(
-      <table className='Board'>
+      <div>
+        <div className='Board-title'>
+          <div className="neon-orange">Lights</div>
+          <div className="neon-blue">Out</div>
+        </div>
+        <table className='Board'>
         <tbody>
           {tableBoard}
         </tbody>
       </table>
+      </div>
     )
   }
 }
